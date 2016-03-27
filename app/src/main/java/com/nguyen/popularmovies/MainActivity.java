40759ab98a66b174215c3fb341ac91357c5410a7
iceguyvn <nguyen_my@yahoo.com>
2016-03-26 17:35:14 -0700

    moved code that update the RecyclerView outside of JsonHttpResponseHandler and use notifyItemRangeRemoved() and notifyItemRangeInserted() instead

--- app/src/main/java/com/nguyen/popularmovies/MainActivity.java
+++ app/src/main/java/com/nguyen/popularmovies/MainActivity.java
@@ -11,39 +11,53 @@ import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
+   // should this be a singleton?
    TMDBClient mClient = new TMDBClient();
-   List<Movie> mMovies = null;
+   // data source, which needs to be an empty ArrayList and not NULL
+   List<Movie> mMovies = new ArrayList<>();
+   // bind data source to adapter
+   RecyclerViewAdapter mAdapter = new RecyclerViewAdapter(mMovies);
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_main);
 
+      /*
+      // unsuccessful attempt at using GridView and custom ArrayAdapter (MoviesAdapter)
+      // create the adapter to convert the array to views
+      MoviesAdapter adapter = new MoviesAdapter(MainActivity.this, mMovies);
+      // attach the adapter to a ListView
+      GridView listView = (GridView) findViewById(R.id.grid_view);
+      listView.setAdapter(adapter);
+      */
+      // look up the RecyclerView in activity layout
+      RecyclerView listView = (RecyclerView)findViewById(R.id.recycler_view);
+      // attach the adapter to the RecyclerView to populate items
+      listView.setAdapter(mAdapter);
+      // set layout manager to position the items
+      listView.setLayoutManager(new GridLayoutManager(MainActivity.this, 2));
+      // load data into view
+      getPopularMovies();
+   }
+
+   private void getPopularMovies() {
       mClient.getPopularMovies(new JsonHttpResponseHandler() {
          @Override
          public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-            // construct the data source
-            mMovies = Movie.fromJSONArray(response);
-            /*
-            // create the adapter to convert the array to views
-            MoviesAdapter adapter = new MoviesAdapter(MainActivity.this, mMovies);
-            // attach the adapter to a ListView
-            GridView listView = (GridView) findViewById(R.id.grid_view);
-            listView.setAdapter(adapter);
-            */
-            // look up the RecyclerView in activity layout
-            RecyclerView listView = (RecyclerView)findViewById(R.id.recycler_view);
-            // create adapter passing in the sample data
-            RecyclerViewAdapter adapter = new RecyclerViewAdapter(mMovies);
-            // attach the adapter to the RecyclerView to populate items
-            listView.setAdapter(adapter);
-            // set layout manager to position the items
-            listView.setLayoutManager(new GridLayoutManager(MainActivity.this, 2));
+            // empty the current list
+            int size = mAdapter.getItemCount();
+            mMovies.clear();
+            mAdapter.notifyItemRangeRemoved(0, size);
+            // add all new movies to the empty list
+            mMovies.addAll(Movie.fromJSONArray(response));
+            mAdapter.notifyItemRangeInserted(0, mMovies.size());
          }
       });
    }
